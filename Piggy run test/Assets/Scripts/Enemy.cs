0d2ad855e525a80726fc3a71996085b71d5495eb
GregoryAtom22 <62352393+GregoryAtom22@users.noreply.github.com>
2022-04-20 00:47:54 +0600

    dog added

---
+++ Piggy run test/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetInteger("State", 1);
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+    }
+}
